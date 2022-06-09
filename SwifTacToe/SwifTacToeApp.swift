14786a4aac32c2b3b281ae4d3cb3b919c927e46d
Tyler <tyler@tylerbarnes.io>
2022-06-09 12:47:18 -0600

    Initial Commit

---
+++ SwifTacToe/SwifTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifTacToeApp.swift
+//  SwifTacToe
+//
+//  Created by Tyler Barnes on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
